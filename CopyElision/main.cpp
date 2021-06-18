fa527314648cdbfb485593fbff5f7e43ec892f75
yuvaram <yuvaram.aligeti@gmail.com>
2021-06-18 18:53:05 +0530

    Add files via upload

---
+++ CopyElision/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+using namespace std;
+class MyClass {
+   public:
+      MyClass(const char* str = "\0") {  //default constructor
+         cout << "Constructor called" << endl;
+      }
+      MyClass(const MyClass &my_cls) { //copy constructor
+         cout << "Copy constructor called" << endl;
+      }
+};
+
+int main()
+{
+    MyClass ob = "copy class object";
+    return 0;
+}
